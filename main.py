91fe07897dc42d3d8bc9f26d556ab210af4cb309
Robatortas <robatortas1@gmail.com>
2023-05-22 14:54:14 -0700

    GUI BETA

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ import PySimpleGUI as gui
 class Interface:
     title = "Coffee Beans"
     font = ("Cascadia Code", 40)
+    margins = (50, 50)
 
-    backgroundColor = '#ff00ff'
-    layout = [[gui.Text("Coffee Beans", size=(0, 0), key="-text-", font=font)], [gui.Text("Save path")], [gui.In(size=(25, 1), enable_events=True, key="-CHOSEN_PATH-")], [gui.FolderBrowse()]]
+    backgroundColor = '#6D5D46'
+    gui.theme = backgroundColor
 
-    margins = (200, 50)
+    buttonColor = '#917B5D'
 
-
-    gui.Window(title, layout, margins=(200, 200), background_color=backgroundColor).read()
-
+    layout = [[gui.Text("Coffee Beans", size=(0, 0), key="TITLE", font=font, background_color=backgroundColor)], [gui.Text("Save path", background_color=backgroundColor)], [gui.In(size=(25, 1), enable_events=True, key="CHOSEN_PATH")], [gui.FolderBrowse(button_color=buttonColor, key="BROWSE")]]
 
 
+    gui.Window(title, layout, margins=margins, background_color=backgroundColor).read()
+
+class Script:
+    while True:
+        event, values = gui.Window.read()
+        if(event == "BROWSE"):
+            values["CHOSEN_PATH"] 
