59eaaf40b3b1e355d3d5280db16cf6a77a1d5716
Robatortas <robatortas1@gmail.com>
2023-05-22 16:33:31 -0700

    Download Working

--- main.py
+++ main.py
@@ -13,14 +13,34 @@ gui.theme = backgroundColor
 
 buttonColor = '#917B5D'
 
-layout = [[gui.Text("Coffee Beans", size=(0, 0), key="TITLE", font=font, background_color=backgroundColor)], [gui.Text("Save path", background_color=backgroundColor)], [gui.In(size=(25, 1), enable_events=True, key="CHOSEN_PATH"), gui.FolderBrowse(button_color=buttonColor, key="BROWSE")]]
+layout = [[gui.Text("Coffee Beans", size=(0, 0), key="TITLE", font=font, background_color=backgroundColor)], 
+          [gui.Text("Link", background_color=backgroundColor)], 
+          [gui.In(size=(25, 1), enable_events=True, key="LINK")], 
+          [gui.Text("Save path", background_color=backgroundColor)], 
+          [gui.In(size=(25, 1), enable_events=True, key="CHOSEN_PATH"), gui.FolderBrowse(button_color=buttonColor, key="BROWSE")],
+          [gui.Button("DOWNLOAD", button_color='#CA8957')]]
+
 window = gui.Window(title, layout, margins=margins, background_color=backgroundColor)
 
 class Script:
     while True:
         event, values = window.read()
         if(event == "Exit" or event == gui.WINDOW_CLOSED): break
+        
+        if(event == "DOWNLOAD"):
+            print("DOWNLOAD STARTED")
 
-        if(event == "BROWSE"):
-            print("BROWSE")
-            window["CHOSEN_PATH"].update(window["CHOSEN_PATH"].get() + "Hey")
+            downloadLink = values['LINK']
+            print("var downloadLink = " + downloadLink)
+
+            
+            if(downloadLink != None or not str(downloadLink).startswith("https://www.youtube.com/watch")):
+                print("\nERROR: Not a valid YouTube link.\n")
+            else: print("\nStarting Conversion")
+
+           # window["CHOSEN_PATH"].update(window["CHOSEN_PATH"].get() + "Hey")
+
+            YouTube(downloadLink).streams.first().download()
+            yt = YouTube(downloadLink)
+            yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download()
+            
