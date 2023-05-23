f38bfa2f67b26f498db4f502cecd6dd581a3a49e
Robatortas <robatortas1@gmail.com>
2023-05-23 15:13:09 -0700

    Finished Executable

--- Coffee Beans EXE/Coffee Beans/Coffee Beans/Main.cpp
+++ Coffee Beans EXE/Coffee Beans/Coffee Beans/Main.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
 #include <filesystem>
-#include "Python.h"
+#include <Windows.h>
 
-void program(const char* path) {
+void program() {
 	std::cout << "Running Coffee Beans";
-	
-	//Py_Initialize();
-	//PyRun_SimpleString("import os");
-	//PyRun_SimpleString("path = \"python \" + str(os.getcwd()) + \"\\scripts\\Coffee_Beans.py\"");
-	//PyRun_SimpleString("print(\"python \" + str(os.getcwd()) + \"\\scripts\\Coffee_Beans.py\")");
-	//Py_Finalize();
-	std::string command = "python " + std::string(path) + "main.py";
-	//const char* command = "python";
+	std::filesystem::path path = std::filesystem::current_path();
+	std::string command = "python \"" + path.string() + "\\internal\\coffee_beans.py\"";
+	std::cout << "\n\nWILL BE RUNNING APPLICATION AT: " << command << "\n\n";
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+	std::system(std::string(command).c_str());
+	return;
 }
 
-// COUNTS CHARS ON STRING
-int countChars(const char* string) {
-
-	int size = sizeof(string);
-	int count = size;
-	std::cout << count;
-	std::cout << string;
-	return count;
-}
-
-int main(int argc, char* argv[]) {
-	//program(argv[0]);
-	countChars("Heyaaaaaaaaaaaaa");
-	std::cin.get();
+int main() {
+	program();
 	return 0;
 }
