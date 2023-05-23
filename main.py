6fdfd4547aa130ad560f4947c9be54c4e3a4c1c5
Robatortas <robatortas1@gmail.com>
2023-05-22 17:54:23 -0700

    GUI WARNINGS

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ buttonColor = '#917B5D'
 layout = [[gui.Text("Coffee Beans", size=(0, 0), key="TITLE", font=font, background_color=backgroundColor)], 
           [gui.Text("Link", background_color=backgroundColor)], 
           [gui.In(size=(25, 1), enable_events=True, key="LINK")], 
-          [gui.Text("Save path", background_color=backgroundColor)], 
+          [gui.Text("Download path", background_color=backgroundColor)], 
           [gui.In(size=(25, 1), enable_events=True, key="CHOSEN_PATH"), gui.FolderBrowse(button_color=buttonColor, key="BROWSE")],
           [gui.Button("DOWNLOAD", button_color='#CA8957')]]
 
@@ -25,10 +25,10 @@ window = gui.Window(title, layout, margins=margins, background_color=backgroundC
 
 class Script:
 
-    def warning():
-        layoutW = [[gui.Text("WARNING: The program will freeze, that's okay!\nDon't fret, I am working hard on converting your video.", key="WARNING_WIN")], [gui.Button("Ok", key="OK_WARN")]]
+    def warning(title, text):
+        layoutW = [[gui.Text(text, key="WARNING_WIN")], [gui.Button("Ok", key="OK_WARN")]]
 
-        window = gui.Window("WARNING", layoutW, modal=True)
+        window = gui.Window(title, layoutW, modal=True)
 
         choice = None
 
@@ -55,13 +55,15 @@ class Script:
                     try:
                         window.set_title(title + " || CONVERTING")
                         yt = YouTube(downloadLink)
-                        warning()
+                        warning("WARNING", "WARNING: The program will freeze, that's okay!\nDon't fret, I am working hard on converting your video.")
                         print("var downloadLink = " + downloadLink)
                         print("\nCONVERTING AND DOWNLOADING\n")
                         yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(chosenPath)
                     except ExtractError:
                         print("\nERROR: INVALID YOUTUBE LINK\n")
+                        warning("ERROR", "ERROR: INVALID YOUTUBE LINK.\n\nPLEASE PROMPT A VALID ONE.")
                     
                     break
-                else: print("\nERROR: INVALID PATH\n")
-            
+                else: 
+                    print("\nERROR: INVALID DOWNLOAD PATH\n")
+                    warning("ERROR", "ERROR: INVALID DOWNLOAD PATH.\n\nPLEASE PROMPT A VALID ONE.")
